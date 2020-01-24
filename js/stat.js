a745576fc710f2ee830ebc1e3701227d4f650cf2
nmrhard <nmrhard@gmail.com>
2020-01-24 17:22:23 +0200

    Fix code style

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,3 @@
-'use srtict';
-
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -9,19 +7,18 @@ var FINAL_TEXT_X = 120;
 var FINAL_TEXT_Y = 40;
 var FINAL_TEXT_GAP = 20;
 var BAR_X = 140;
-var BAR_Y = 90;
 var BAR_WIDTH = 40;
 var MAX_BAR_HEIGHT = 150;
 var BAR_GAP = 50;
 var NAME_GAP = 10;
 var NAME_HEIGHT = 20;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
-var getMaxTime = function(times) {
+var getMaxTime = function (times) {
   if (times.length === 0) {
     times[0] = 0;
   }
@@ -35,7 +32,7 @@ var getMaxTime = function(times) {
   }
 
   return maxTime;
-}
+};
 
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
@@ -46,17 +43,17 @@ window.renderStatistics = function (ctx, players, times) {
   for (var i = 0; i < players.length; i++) {
     var barHeight = (MAX_BAR_HEIGHT * times[i]) / maxTime;
 
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = '#000';
     ctx.fillText(players[i], BAR_X + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - NAME_GAP);
     ctx.fillText(Math.floor(times[i]), BAR_X + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - NAME_GAP * 2 - NAME_HEIGHT - barHeight);
-    ctx.fillStyle = 'hsl(240,' + Math.floor(Math.random()*100)  + '%, 50%)';
+    ctx.fillStyle = 'hsl(240,' + Math.floor(Math.random() * 100) + '%, 50%)';
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
     ctx.fillRect(BAR_X + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - NAME_GAP - NAME_HEIGHT - barHeight, BAR_WIDTH, barHeight);
   }
 
-  ctx.fillStyle = "#000";
+  ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', FINAL_TEXT_X, FINAL_TEXT_Y);
   ctx.fillText('Список результатов: ', FINAL_TEXT_X, FINAL_TEXT_Y + FINAL_TEXT_GAP);
