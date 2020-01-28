c0b6d019dd237da7e83ec336282af9f68d952fc0
nmrhard <nmrhard@gmail.com>
2020-01-28 20:31:32 +0200

    Add function render characters and function add to DOM

--- js/setup.js
+++ js/setup.js
@@ -1,37 +1,68 @@
 'use strict';
 
-var FIRST_NAME = ['Иван','Хуан Себастьян','Мария','Кристоф','Виктор','Юлия','Люпита','Вашингтон'];
-var LAST_NAME = ['да Марья','Верон','Мирабелла','Вальц','Онопко','Топольницкая','Нионго','Ирвинг'];
-var COAT_COLOR = ['rgb (101, 137, 164)','rgb (241, 43, 107)','rgb (146, 100, 161)','rgb (56, 159, 117)','rgb (215, 210, 55)','rgb (0, 0, 0)'];
-var EYES_COLOR = ['black','red','blue','yellow','green'];
+var FIRST_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var LAST_NAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var NUMBER_CHARACTERS = 4;
-var characters = [];
 
-document.querySelector('.setup').classList.remove('hidden');
-document.querySelector('.setup-similar').classList.remove('hidden');
+var setupWindow = document.querySelector('.setup');
+setupWindow.classList.remove('hidden');
 
-var getRandomElement = function(arr) {
-  return arr[Math.floor(Math.random() * arr.length)]
-}
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarCharacterTemplate = document.querySelector('#similar-wizard-template')
+.content
+.querySelector('.setup-similar-item');
+
+var getRandomElement = function (arrElements) {
+  return arrElements[Math.floor(Math.random() * arrElements.length)];
+};
 
 var generateCharacter = function () {
-  var _firstName = getRandomElement(FIRST_NAME);
-  var _lastName = getRandomElement(LAST_NAME);
-  var _coatColor = getRandomElement(COAT_COLOR);
-  var _eyesColor = getRandomElement(EYES_COLOR);
+  var firstName = getRandomElement(FIRST_NAME);
+  var lastName = getRandomElement(LAST_NAME);
+  var coatColor = getRandomElement(COAT_COLOR);
+  var eyesColor = getRandomElement(EYES_COLOR);
 
   var character = {
-    name: _firstName + ' ' + _lastName,
-    coatColor: _coatColor,
-    eyesColor: _eyesColor
+    name: firstName + ' ' + lastName,
+    coatColor: coatColor,
+    eyesColor: eyesColor
   };
 
   return character;
-}
+};
 
-for (var i = 0; i < NUMBER_CHARACTERS; i++) {
-  var character = generateCharacter();
-  characters.push(character);
-}
+var createCharacters = function () {
+  var characters = [];
 
+  for (var i = 0; i < NUMBER_CHARACTERS; i++) {
+    characters.push(generateCharacter());
+  }
 
+  return characters;
+};
+
+var renderCharacter = function (character) {
+  var characterElement = similarCharacterTemplate.cloneNode(true);
+
+  characterElement.querySelector('.setup-similar-label').textContent = character.name;
+  characterElement.querySelector('.wizard-coat').style.fill = character.coatColor;
+  characterElement.querySelector('.wizard-eyes').style.fill = character.eyesColor;
+
+  return characterElement;
+};
+
+var addCharactersToList = function (characters) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < characters.length; i++) {
+    fragment.appendChild(renderCharacter(characters[i]));
+  }
+
+  return fragment;
+};
+
+similarListElement.appendChild(addCharactersToList(createCharacters()));
+
+setupWindow.querySelector('.setup-similar').classList.remove('hidden');
