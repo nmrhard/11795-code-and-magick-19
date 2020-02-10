9a731f5c9e6abfee571508437b65ae8a77702c3f
nmrhard <nmrhard@gmail.com>
2020-02-10 12:00:47 +0200

    Create nodes module

---
+++ js/nodes.js
@@ -0,0 +1,10 @@
+'use struct';
+
+(function () {
+  window.nodes = {
+    SIMILAR_LIST_ELEMENT: document.querySelector('.setup-similar-list'),
+    SIMILAR_CHARACTER_TEMPLATE: document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item'),
+    SETUP_WINDOW: document.querySelector('.setup'),
+    SETUP_OPEN: document.querySelector('.setup-open')
+  };
+})();
