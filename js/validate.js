48ebdf4fe00f9d4793cec2da1e019d57be86e4fa
nmrhard <nmrhard@gmail.com>
2020-02-10 12:01:40 +0200

    Create validate module

---
+++ js/validate.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function () {
+  var validateMessage = function (validate) {
+    var message = '';
+
+    if (validate.tooShort) {
+      message = 'Имя должно состоять минимум из 2-х символов';
+    } else if (validate.tooLong) {
+      message = 'Имя не должно превышать 25-ти символов';
+    } else if (validate.valueMissing) {
+      message = 'Обязательное поле';
+    }
+
+    return message;
+  };
+
+  var onInputValidate = function () {
+    var message = validateMessage(window.setupNodes.USER_NAME_INPUT.validity);
+    window.setupNodes.USER_NAME_INPUT.setCustomValidity(message);
+  };
+
+  window.validate = function (element) {
+    element.addEventListener('invalid', onInputValidate);
+  }
+})();
